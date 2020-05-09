a224ebee685aa65558c0a3d6d4e40056e9cd8725
Soonmok Kwon <soonmok.kwon@voyagerx.com>
2020-05-09 20:58:16 +0900

    add sample_neg function

--- data_loader/Interaction.py
+++ data_loader/Interaction.py
@@ -1,7 +1,25 @@
-import numpy as np
 import json
-from sklearn.preprocessing import LabelEncoder
+
+import numpy as np
+from scipy import sparse as sp
 from scipy.sparse import csr_matrix
+from sklearn.preprocessing import LabelEncoder
+
+
+def sample_neg(positive_interaction, neg_sample_ratio=1.2):
+    num_neg_sample = int(positive_interaction.size * neg_sample_ratio)
+    Y = sp.coo_matrix(positive_interaction.shape)
+    Y_size = 0
+    num_rows, num_cols = positive_interaction.shape
+    while Y_size < num_neg_sample:
+        num_to_sample = num_neg_sample - Y_size
+        data = np.concatenate([Y.data, np.ones(num_to_sample)])
+        Y_row = np.concatenate([Y.row, np.random.choice(num_rows, num_to_sample)])
+        Y_col = np.concatenate([Y.col, np.random.choice(num_cols, num_to_sample)])
+        Y = sp.coo_matrix((data, (Y_row, Y_col)), shape=positive_interaction.shape)
+        Y = sp.coo_matrix(Y - positive_interaction.multiply(Y))
+        Y_size = Y.size
+    return Y
 
 
 class Interaction(object):
@@ -32,11 +50,12 @@ class Interaction(object):
         return csr_matrix((data, (self.playlist_ids, self.song_ids)))
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     song_meta_file = "../res/song_meta.json"
     train_file = "../res/train.json"
     with open(train_file) as f:
         data = json.load(f)
     interaction = Interaction(data)
     matrix = interaction.build_interaction_matrix()
+    neg_matrix = sample_neg(matrix, neg_sample_ratio=1.2)
     print("finish")
