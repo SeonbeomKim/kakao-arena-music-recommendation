4da8b4b0021f5ace1ad2b5510d24c97f53aba1b9
SeonbeomKim <kymj1@naver.com>
2020-05-20 17:15:19 +0900

    bucket 데이터 처리하도록 변환

--- trainer/session_trainer.py
+++ trainer/session_trainer.py
@@ -2,81 +2,73 @@ import os
 
 import numpy as np
 import tensorflow as tf
-from data_loader.Session_interaction import Session
+import util
+# from data_loader.Session_interaction import Session
 from models.Transformer import Transformer
-from tqdm import *
-from util import load_json, fill_na
+from tqdm import tqdm
 
 
-def train(model, session, lr, batch_size=64, keep_prob=0.9, pad_symbol='@pad'):
+def train(model, session, lr, batch_size=64, keep_prob=0.9):
     loss = 0
-    data_num = len(session.model_input_A_length)
 
-    shuffle_index = np.array(range(data_num))
-    np.random.shuffle(shuffle_index)
+    total_data_num = 0
+    for bucket in session:
+        data_num = len(session[bucket]['model_input_A_length'])
+        total_data_num += data_num
+        shuffle_index = np.array(range(data_num))
+        np.random.shuffle(shuffle_index)
 
-    session.model_input = session.model_input[shuffle_index]
-    session.model_input_A_length = session.model_input_A_length[shuffle_index]
-    session.positive_label = session.positive_label[shuffle_index]
-    session.negative_label = session.negative_label[shuffle_index]
+        session[bucket]['model_input'] = session[bucket]['model_input'][shuffle_index]
+        session[bucket]['model_input_A_length'] = session[bucket]['model_input_A_length'][shuffle_index]
+        session[bucket]['positive_label'] = session[bucket]['positive_label'][shuffle_index]
+        session[bucket]['negative_label'] = session[bucket]['negative_label'][shuffle_index]
 
-    epoch = int(np.ceil(data_num / batch_size))
-
-    for i in tqdm_notebook(range(epoch)):  # TODO tqdm
-        input_sequence_indices = session.model_input[batch_size * i: batch_size * (i + 1)]
-        # 가장 긴 길이에 맞게 pad
-
-        input_sequence_indices = fill_na(
-            input_sequence_indices.tolist(),
-            session.label_encoder.transform([pad_symbol])[0]).astype(np.int32)
-
-        try:
+        epoch = int(np.ceil(data_num / batch_size))
+        for i in tqdm(range(epoch)):
             _, train_loss = sess.run([model.minimize, model.loss],
-                                     {model.input_sequence_indices: input_sequence_indices,
-                                      model.A_length: session.model_input_A_length[
+                                     {model.input_sequence_indices: session[bucket]['model_input'][
+                                                                    batch_size * i: batch_size * (i + 1)],
+                                      model.A_length: session[bucket]['model_input_A_length'][
                                                       batch_size * i: batch_size * (i + 1)],
-                                      model.next_item_idx: session.positive_label[batch_size * i: batch_size * (i + 1)],
-                                      model.negative_item_idx: session.negative_label[
+                                      model.next_item_idx: session[bucket]['positive_label'][
+                                                           batch_size * i: batch_size * (i + 1)],
+                                      model.negative_item_idx: session[bucket]['negative_label'][
                                                                batch_size * i: batch_size * (i + 1)],
                                       model.keep_prob: keep_prob,
                                       model.lr: lr
                                       })
             loss += train_loss
-        except Exception as e:
-            print(e)
-            print(len(session.model_input[batch_size * i: batch_size * (i + 1)]))
-            print(input_sequence_indices.shape)
-            print(input_sequence_indices)
 
-    return loss / data_num
+    return loss / total_data_num
 
 
-def validation(model, session, batch_size=64, pad_symbol='@pad'):
+def validation(model, session, batch_size=64):
     loss = 0
 
-    data_num = len(session.model_input_A_length)
-    epoch = int(np.ceil(data_num / batch_size))
+    total_data_num = 0
+    for bucket in session:
+        data_num = len(session[bucket]['model_input_A_length'])
+        total_data_num += data_num
 
-    for i in tqdm(range(epoch)):  # TODO tqdm
-        input_sequence_indices = session.model_input[batch_size * i: batch_size * (i + 1)]
-        # 가장 긴 길이에 맞게 pad
-        input_sequence_indices = fill_na(
-            input_sequence_indices.tolist(),
-            session.label_encoder.transform([pad_symbol])[0]).astype(np.int32)
+        epoch = int(np.ceil(data_num / batch_size))
+        for i in tqdm(range(epoch)):
+            val_loss = sess.run(model.loss,
+                                {model.input_sequence_indices: session[bucket]['model_input'][
+                                                               batch_size * i: batch_size * (i + 1)],
+                                 model.A_length: session[bucket]['model_input_A_length'][
+                                                 batch_size * i: batch_size * (i + 1)],
+                                 model.next_item_idx: session[bucket]['positive_label'][
+                                                      batch_size * i: batch_size * (i + 1)],
+                                 model.negative_item_idx: session[bucket]['negative_label'][
+                                                          batch_size * i: batch_size * (i + 1)],
+                                 model.keep_prob: 1.0,
+                                 })
+            loss += val_loss
 
-        val_loss = sess.run(model.loss,
-                            {model.input_sequence_indices: input_sequence_indices,
-                             model.A_length: session.model_input_A_length[batch_size * i: batch_size * (i + 1)],
-                             model.next_item_idx: session.positive_label[batch_size * i: batch_size * (i + 1)],
-                             model.negative_item_idx: session.negative_label[batch_size * i: batch_size * (i + 1)],
-                             model.keep_prob: 1.0,
-                             })
-        loss += val_loss
-
-    return loss / data_num
+    return loss / total_data_num
 
 
-def run(model, train_session, val_session, saver_path, lr, batch_size=512, keep_prob=0.9, pad_symbol='@pad', restore=0):
+def run(model, sess, saver, train_session, val_session, saver_path, lr, batch_size=512, keep_prob=0.9, restore=0):
     if not os.path.exists(saver_path):
         print("create save directory")
         os.makedirs(saver_path)
@@ -85,26 +77,27 @@ def run(model, train_session, val_session, saver_path, lr, batch_size=512, keep_
         print("create save directory")
         os.makedirs(os.path.join(saver_path, 'tensorboard'))
 
-    #     if restore != 0:
-    #         model.saver.restore(sess, saver_path + str(restore) + ".ckpt")
-    #         print('restore:', restore)
+    if restore != 0:
+        model.saver.restore(sess, saver_path + str(restore) + ".ckpt")
+        print('restore:', restore)
+    else:
+        with tf.name_scope("tensorboard"):
+            train_loss_tensorboard = tf.placeholder(tf.float32, name='train_loss')  # with regularization (minimize 할 값)
+            valid_loss_tensorboard = tf.placeholder(tf.float32, name='valid_loss')  # no regularization
 
-    with tf.name_scope("tensorboard"):
-        train_loss_tensorboard = tf.placeholder(tf.float32, name='train_loss')  # with regularization (minimize 할 값)
-        valid_loss_tensorboard = tf.placeholder(tf.float32, name='valid_loss')  # no regularization
+            train_loss_summary = tf.summary.scalar("train_loss", train_loss_tensorboard)
+            valid_loss_summary = tf.summary.scalar("valid_loss", valid_loss_tensorboard)
 
-        train_loss_summary = tf.summary.scalar("train_loss", train_loss_tensorboard)
-        valid_loss_summary = tf.summary.scalar("valid_loss", valid_loss_tensorboard)
+            merged = tf.summary.merge_all()
+            writer = tf.summary.FileWriter(os.path.join(saver_path, 'tensorboard'), sess.graph)
+            # merged_train_valid = tf.summary.merge([train_summary, valid_summary])
+            # merged_test = tf.summary.merge([test_summary])
 
-        merged = tf.summary.merge_all()
-        writer = tf.summary.FileWriter(os.path.join(saver_path, 'tensorboard'), sess.graph)
-        # merged_train_valid = tf.summary.merge([train_summary, valid_summary])
-        # merged_test = tf.summary.merge([test_summary])
-
-    for epoch in range(restore + 1, 150 + 1):
-        train_loss = train(model, train_session, lr, batch_size=batch_size, keep_prob=keep_prob, pad_symbol=pad_symbol)
+    print("epoch: %d, valid_loss: %f" % (0, valid_loss))
+    for epoch in range(restore + 1, 200 + 1):
+        train_loss = train(model, train_session, lr, batch_size=batch_size, keep_prob=keep_prob)
         print("epoch: %d, train_loss: %f" % (epoch, train_loss))
-        valid_loss = validation(model, val_session, batch_size=batch_size, pad_symbol=pad_symbol)
+        valid_loss = validation(model, val_session, batch_size=batch_size)
         print("epoch: %d, valid_loss: %f" % (epoch, valid_loss))
         print()
 
@@ -114,57 +107,50 @@ def run(model, train_session, val_session, saver_path, lr, batch_size=512, keep_
             valid_loss_tensorboard: valid_loss,
         })
         writer.add_summary(summary, epoch)
+        # if (epoch) % 5 == 0:
+        print('save model')
+        saver.save(sess, os.path.join(saver_path, str(epoch) + ".ckpt"))
 
 
 # make train / val set
-train_set = load_json('dataset/orig/train.json')
-train_session = Session(train_set)
-train_session.set_label_encoder()
-train_session.make_dataset(positive_k=3, negative_k=10, sample_num_of_each_plylst=1)
-
-val_set = load_json('dataset/orig/val.json')
-val_session = Session(val_set, train_session.label_encoder)
-val_session.set_label_encoder()
-val_session.make_dataset(positive_k=3, negative_k=10, sample_num_of_each_plylst=1)
-
-# dataset to numpy
-train_session.model_input = np.array(train_session.model_input)  # row마다 column수가 달라서 dtype 줄 수 없음
-train_session.model_input_A_length = np.array(train_session.model_input_A_length, np.int32)
-train_session.positive_label = np.array(train_session.positive_label, np.int32)
-train_session.negative_label = np.array(train_session.negative_label, np.int32)
-
-val_session.model_input = np.array(val_session.model_input)
-val_session.model_input_A_length = np.array(val_session.model_input_A_length, np.int32)
-val_session.positive_label = np.array(val_session.positive_label, np.int32)
-val_session.negative_label = np.array(val_session.negative_label, np.int32)
+train_session = util.load('./model_train_dataset_bucket.pickle')
+val_session = util.load('./model_val_dataset_bucket.pickle')
+label_encoder = util.load('./label_encoder.pickle')
+total_songs = label_encoder.total_songs  # 나중에 playlist 임베딩이랑 내적할때 total_songs에 대해서만 수행
+label_encoder = label_encoder.label_encoder
+pad_symbol = '@pad'
 
 # model
 model = Transformer(
-    voca_size=len(train_session.label_encoder.classes_),
-    embedding_size=12,
+    voca_size=len(label_encoder.classes_),
+    embedding_size=64,  # 128인경우 850MB 먹음.
     is_embedding_scale=True,
     max_sequence_length=107,
-    encoder_decoder_stack=1,
+    encoder_decoder_stack=3,
     multihead_num=4,
-    pad_idx=train_session.label_encoder.transform(['@pad'])[0],
-    cls_idx=train_session.label_encoder.transform(['@cls'])[0])
+    pad_idx=label_encoder.transform(['@pad'])[0],
+    cls_idx=label_encoder.transform(['@cls'])[0])
 
 # gpu 할당 및 session 생성
-k = 1
+k = 6
 os.environ["CUDA_VISIBLE_DEVICES"] = str(k)  # nvidia-smi의 k번째 gpu만 사용
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True  # 필요한 만큼만 gpu 메모리 사용
+
 sess = tf.Session(config=config)
+saver = tf.train.Saver(max_to_keep=10000)
+
 sess.run(tf.global_variables_initializer())
 
 # # 학습 진행
 run(
     model,
+    sess,
+    saver,
     train_session,
     val_session,
-    saver_path='./',
+    saver_path='./saver_emb64_stack3_head4_lr_1e-3',
     lr=1e-3,
-    batch_size=32,
+    batch_size=512,
     keep_prob=0.9,
-    pad_symbol='@pad',
     restore=0)
