47b7c501fadf1921236ffa1a6d744765cfbc404a
Hyunje Jo <retriever89@gmail.com>
2020-05-25 21:13:31 +0900

    테스트 코드 추가

--- data_loader/song_info_loader.py
+++ data_loader/song_info_loader.py
@@ -13,7 +13,7 @@ class SongMetaInfoLoader(object):
         self.song_album_name_map = {}
         self.song_artist_map = {}
 
-    def make_song_tag_info(self, playlist_data, top_n):
+    def _make_song_tag_info(self, playlist_data, top_n):
         song_tag_dict = defaultdict(list)
         for playlist in tqdm(playlist_data):
             tag = playlist['tags']
@@ -26,7 +26,7 @@ class SongMetaInfoLoader(object):
             tag_counter = Counter(tag_list).most_common(top_n)
             self.song_tag_map[song_id] = list(map(lambda kv: kv[0], tag_counter))
 
-    def make_song_metainfo_map(self, meta_data, top_n):
+    def _make_song_metainfo_map(self, meta_data, top_n):
         for meta in tqdm(meta_data):
             song_id = meta['id']
             self.song_artist_map[song_id] = meta['artist_id_basket'][:top_n]
@@ -42,8 +42,8 @@ class SongMetaInfoLoader(object):
 
     def make_info_map(self, playlist_data, meta_data, top_n=5):
         print('make meta info of songs!')
-        self.make_song_tag_info(playlist_data=playlist_data, top_n=top_n)
-        self.make_song_metainfo_map(meta_data=meta_data, top_n=5)
+        self._make_song_tag_info(playlist_data=playlist_data, top_n=top_n)
+        self._make_song_metainfo_map(meta_data=meta_data, top_n=5)
         print('done!')
 
 
@@ -52,3 +52,11 @@ if __name__ == '__main__':
     meta_info = load_json('../res/song_meta.json')
     meta_info_loader = SongMetaInfoLoader()
     meta_info_loader.make_info_map(playlist_data=playlist_info, meta_data=meta_info)
+
+    test_song_id = 17
+    print(meta_info_loader.song_tag_map[test_song_id])
+    print(meta_info_loader.song_genre_map[test_song_id])
+    print(meta_info_loader.song_detail_genre_map[test_song_id])
+    print(meta_info_loader.song_album_id_map[test_song_id])
+    print(meta_info_loader.song_album_name_map[test_song_id])
+    print(meta_info_loader.song_artist_map[test_song_id])
