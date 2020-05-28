ed05c8c4cb7d83008d3483ca7f5a1613ac2d5f88
SeonbeomKim <kymj1@naver.com>
2020-05-28 20:00:53 +0900

    songs, tags 기반의 wmf 학습셋 생성

--- data_loader/dump_songs_tags_wmf_dataset.py
+++ data_loader/dump_songs_tags_wmf_dataset.py
@@ -1,13 +1,9 @@
 import os
 
-import implicit
-
-from data_loader.Label_info import Label_info
 import data_loader.songs_tags_wmf_util as songs_tags_wmf_util
 import parameters
 import util
-
-
+from data_loader.Label_info import Label_info
 
 if __name__ == "__main__":
     train_set = util.load_json('dataset/orig/train.json')
@@ -19,18 +15,9 @@ if __name__ == "__main__":
         label_info = Label_info(train_set)
         util.dump(label_info, label_info_path)
 
-
     csr = songs_tags_wmf_util.make_sparse_matrix(  # tags-songs matrix
         train_set,
         label_info.label_encoder,
         len(label_info.songs),
         label_info.label_encoder.transform([label_info.unk_token])[0])
     util.dump(csr, os.path.join(parameters.base_dir, parameters.songs_tags_csr_matrix))
-
-
-    # initialize a model
-    model = implicit.als.AlternatingLeastSquares(factors=parameters.embed_size, use_gpu=False)
-
-    # train the model on a sparse matrix of item/user/confidence weights
-    model.fit(csr)  # item-user 순으로 넣어야하는데 우리는 tags-songs이므로 item:tags, user:songs
-    util.dump(model, os.path.join(parameters.base_dir, parameters.songs_tags_wmf))
