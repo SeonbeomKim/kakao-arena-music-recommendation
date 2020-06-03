5b2a28a28d8ce060e3d3302c67fdcd48f51aabf0
SeonbeomKim <kymj1@naver.com>
2020-06-03 20:23:31 +0900

    songs tags util

--- data_loader/songs_tags_util.py
+++ data_loader/songs_tags_util.py
@@ -1,8 +1,10 @@
 import random
 
 import numpy as np
-import util
 from tqdm import tqdm
+
+import parameters
+import util
 
 random.seed(777)
 np.random.seed(777)
@@ -21,6 +23,20 @@ def convert_model_input(songs, tags, label_encoder=None):
         return label_encoder.transform(result)
     return result
 
+
+def get_random_sample(data, min_sample_size, max_sample_size):
+    if not data:
+        return []
+    return random.sample(data, random.randint(min_sample_size, max_sample_size))
+
+
+def get_random_sample2(data, min_dropout=0.4, max_dropout=1):
+    if not data:
+        return []
+
+    keep_ratio = (1 - random.randint(min_dropout * 100, max_dropout * 100 - 1) / 100)
+    keep_num = max(int(len(data) * keep_ratio), 1)
+    return random.sample(data, keep_num)
 
 
 def get_random_sampled_model_input(songs, tags):
@@ -44,55 +60,44 @@ def get_random_sampled_model_input(songs, tags):
     return songs, tags
 
 
-def make_train_val_set(model_input_output, input_bucket_size, output_bucket_size, label_info, sample=5, shuffle=False):
-    bucket_dataset = {}
+def make_train_val_set(model_input_output, model_input_size, label_info, sample=5, shuffle=False):
+    dataset = {"model_input":[], "A_length":[], 'label':[]}
 
-    for each in tqdm(model_input_output, total=len(model_input_output)):
-        songs, tags, positive_label, negative_label = each
-
-        for _ in range(sample):
-            _negative_label = random.sample(negative_label, len(positive_label))
-            songs, tags = get_random_sampled_model_input(songs, tags)
-            model_input = convert_model_input(songs, tags)
-            model_input_A_length = len(songs)+2  # A_length: len(cls || songs || sep)
-
-            input_bucket = util.select_bucket(len(model_input), input_bucket_size)
-            output_bucket = util.select_bucket(len(positive_label), output_bucket_size)
-
-            bucket = (input_bucket, output_bucket)
-            pad_model_input = label_info.label_encoder.transform(
-                model_input + [label_info.pad_token] * (input_bucket - len(model_input)))
-            pad_positive_label = label_info.label_encoder.transform(
-                positive_label + [label_info.unk_token] * (output_bucket - len(positive_label)))
-            pad_negative_label = label_info.label_encoder.transform(
-                _negative_label + [label_info.unk_token] * (output_bucket - len(_negative_label)))
-
-            if bucket not in bucket_dataset:
-                bucket_dataset[bucket] = {}
-                bucket_dataset[bucket]['model_input'] = []
-                bucket_dataset[bucket]['model_input_A_length'] = []
-                bucket_dataset[bucket]['positive_label'] = []
-                bucket_dataset[bucket]['negative_label'] = []
-            bucket_dataset[bucket]['model_input'].append(pad_model_input)
-            bucket_dataset[bucket]['model_input_A_length'].append(model_input_A_length)
-            bucket_dataset[bucket]['positive_label'].append(pad_positive_label)
-            bucket_dataset[bucket]['negative_label'].append(pad_negative_label)
-
-    for bucket in bucket_dataset:
-        for key in bucket_dataset[bucket]:
-            bucket_dataset[bucket][key] = np.array(bucket_dataset[bucket][key], np.int32)
-
+    for _ in range(sample):
         if shuffle:
-            shuffle_index = np.array(range(len(bucket_dataset[bucket]['model_input'])))
-            np.random.shuffle(shuffle_index)
+            random.shuffle(model_input_output)
 
-            for key in bucket_dataset[bucket]:
-                bucket_dataset[bucket][key] = bucket_dataset[bucket][key][shuffle_index]
+        for each in tqdm(model_input_output, total=len(model_input_output)):
+            songs, tags = each
+            label = songs+tags
 
-    return bucket_dataset
+            songs = get_random_sample2(songs)[:parameters.val_test_max_songs]
+            tags = get_random_sample2(tags)[:parameters.val_test_max_tags]
+            if not songs and not tags:
+                continue
+
+            if songs and tags:
+                mode = random.choice(['songs', 'tags', 'all'])
+                if mode == 'songs':
+                    tags = []
+                elif mode == 'tags':
+                    songs = []
+
+            model_input = convert_model_input(songs, tags)
+            A_length = len(songs) + 2  # A_length: len(cls || songs || sep)
+
+            pad_model_input = label_info.label_encoder.transform(
+                model_input + [label_info.pad_token] * (model_input_size - len(model_input)))
+            label = label_info.label_encoder.transform(label)
+
+            dataset["model_input"].append(pad_model_input)
+            dataset["A_length"].append(A_length)
+            dataset["label"].append(label)
+
+    return dataset
 
 
-def make_model_input_output(dataset, label_info):
+def make_model_input_output(dataset, label_info, negative_sample_size=1000):
     model_input_output = []
 
     all_songs_set = set(label_info.songs)
@@ -105,13 +110,5 @@ def make_model_input_output(dataset, label_info):
         if not songs and not tags:
             continue
 
-        positive_label = songs + tags
-
-        negative_songs = util.get_k_negative_label(set(songs), label_info.songs, len(songs) * 3)
-        negative_tags = util.get_k_negative_label(set(tags), label_info.tags, len(tags) * 3)
-        negative_label = negative_songs + negative_tags
-
-        model_input_output.append([songs, tags, positive_label, negative_label])
-
+        model_input_output.append([songs, tags])
     return model_input_output
-
