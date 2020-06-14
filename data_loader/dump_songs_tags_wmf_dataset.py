9b66b00d4e2bee471394331323bfe653b12494db
SeonbeomKim <kymj1@naver.com>
2020-06-14 12:00:22 +0900

    latest version

--- data_loader/dump_songs_tags_wmf_dataset.py
+++ data_loader/dump_songs_tags_wmf_dataset.py
@@ -7,12 +7,13 @@ from data_loader.Label_info import Label_info
 
 if __name__ == "__main__":
     train_set = util.load_json('dataset/orig/train.json')
+    song_meta = util.load_json('dataset/song_meta.json')
 
     label_info_path = os.path.join(parameters.base_dir, parameters.label_info)
     if os.path.exists(label_info_path):
         label_info = util.load(label_info_path)
     else:
-        label_info = Label_info(train_set)
+        label_info = Label_info(train_set, song_meta)
         util.dump(label_info, label_info_path)
 
     csr = songs_tags_wmf_util.make_sparse_matrix(  # tags-songs matrix
