d233173cfda69989c1c02a43a4149e930731e2f4
제프리 <jeffrey@buzzni.com>
2020-07-22 09:10:30 +0900

    title embed size 변경

--- parameters.py
+++ parameters.py
@@ -10,7 +10,7 @@ songs_tags_artists_model_multihead = 2#2
 songs_tags_artists_model_stack = 1
 songs_tags_artists_model_max_sequence_length = 280
 
-title_model_embed_size = 128
+title_model_embed_size = 122
 title_model_multihead = 2#2
 title_model_stack = 2
 title_model_max_sequence_length = 54
