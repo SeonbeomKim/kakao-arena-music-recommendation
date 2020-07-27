2fa5f6d78489540620e1840269f1f065c84d746d
제프리 <jeffrey@buzzni.com>
2020-07-27 11:38:58 +0900

    학습전에 데이터 전처리 코드 실행(로직 수정 X)

--- train.py
+++ train.py
@@ -2,6 +2,9 @@ import os
 
 pwd = os.environ['PWD']
 
+print('data preprocess')
+os.system('python3 %s' % os.path.join(pwd, 'data_loader/dump_datasets.py'))
+
 print('train songs tags artists model')
 os.system('python3 %s' % os.path.join(pwd, 'trainer/songs_tags_artists_model_trainer.py' + ' --gpu=0'))
 
